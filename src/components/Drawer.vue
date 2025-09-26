5978cebdca0a1c20d4184fbd58bdefed43f56137
David Torstenson <davidtorstenson@gmail.com>
2025-09-26 15:20:12 -0700

    Updates content in the drawer

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -3,7 +3,7 @@
       v-model="drawer" 
       temporary
       location="right"
-      width="300"
+      width="360"
       style="background:rgba(0,0,0,0.2);backdrop-filter: blur(10px);"
     >    
       <!-- Header -->
@@ -22,11 +22,15 @@
           >
             <VExpansionPanelTitle collapse-icon="mdi-moon-waxing-crescent" expand-icon="mdi-white-balance-sunny" style="font-size:15px;"><VIcon color="white" style="color:#fff !important;" size="small" class="me-2 text-white">mdi-help-circle</VIcon> What is this?</VExpansionPanelTitle>
             <VExpansionPanelText style="font-size:14px;">
-              What's going on here? It's pretty simple, really.
-              <br /><br />
               In the Book of Psalms, the psalmist writes, "teach us to number our days, that we may gain a heart of wisdom."
               <br /><br />
-              Our time on Earth is short. Every single day is a gift. This is but a humble website to remind you of that.
+              Daycounter is here to help you do that. It's a humble little count-<span class="font-italic">up</span> app in n a world filled with countdown apps.
+              <br/><br/>
+              Which makes sense, honestly. We tend to invest way more mental and emotional energy towards reaching the next big mile marker.
+              <br/><br/>
+              This is meant as a bit of a counterweight to that mentality. We can (and should!) look forward with anticipation. But we should also look back with gratitude for how far we've come.
+              <br /><br />
+              Our time on Earth is short. Every single day is a gift. Let's make each one count.
               <br /><br />
               ===<br /><br/>
               <span class="font-weight-bold">Credits</span><br />
@@ -46,7 +50,8 @@
           >
             <VExpansionPanelTitle collapse-icon="mdi-moon-waxing-crescent" expand-icon="mdi-white-balance-sunny" style="font-size:15px;"><VIcon color="white" style="color:#fff !important;" size="small" class="me-2 text-white">mdi-heart</VIcon> Support</VExpansionPanelTitle>
             <VExpansionPanelText style="font-size:14px;">
-              This site is totally free - nothing to sell you (at least at the moment).<br/><br/>But if you receive any value from stopping by, and would like to <a class="text-white font-weight-bold" href="https://buymeacoffee.com/dtorst" target="_blank">buy me a <span style="font-size:20px;line-height:16px;">☕️</span></a> (or a <span style="font-size:18px;line-height:16px;">🍺</span>), I certainly won't say no!
+              <p class="mb-6">This site is totally free - nothing to sell you (at least at the moment).<br/><br/>But if you receive any value from stopping by, and would like to buy me a <span style="font-size:20px;line-height:16px;">☕️</span> (or a <span style="font-size:18px;line-height:16px;">🍺</span>), I certainly won't say no!</p>
+              <VBtn class="text-lowercase" color="success" block href="https://buymeacoffee.com/dtorst" target="_blank"><span class="text-capitalize">S</span>how some&nbsp;<VIcon color="white" style="color:#fff !important;" size="small" class="me-2 text-white">mdi-heart</VIcon></VBtn>
             </VExpansionPanelText>
           </v-expansion-panel>
         </v-expansion-panels>
@@ -58,7 +63,7 @@
       <VDivider class="my-3" />
       <VRow class="d-flex mb-4 mx-4 justify-space-between">
         <a href="https://github.com/dtorst/daycounter" target="_blank"><VIcon color="white" style="color:#fff !important;" size="small" class="me-2 text-white">mdi-github</VIcon></a>
-        <p class="text-grey-lighten-5 text-center text-subtitle-2">2025 © Daycounter</p>
+        <p class="text-grey-lighten-1 text-center text-subtitle-2">2025 © Daycounter</p>
       </VRow>
       </template>
     </VNavigationDrawer>
